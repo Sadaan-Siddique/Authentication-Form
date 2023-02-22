9239da71d214480a2fc6cdaef1abdc40f50b2d47
Sadaan-Siddique <muhammadsadaan20@gmail.com>
2023-02-22 20:14:11 +0500

    finsih

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -5,8 +5,6 @@ export const AuthContext = createContext();
 function AuthContextProvider({ children }) {
     const [isLoggedIn, setIsLoggedIn] = useState(false)
     const [auth, setAuth] = useState('hello')
-    // const [git, setGit] = useState('hello')
-    // const [loginArr, setLoginArr] = useState([]);
 
     let API_URL = 'https://comfortable-gold-belt.cyclic.app';
     return (
